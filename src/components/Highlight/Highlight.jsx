0e4d414d94cfd682cdf6107c2a9f50e80126262f
knightoffcode <isaiaspereira103@yahoo.com>
2024-10-24 10:14:56 -0300

    #006 - Highlight styles and image it self.

--- src/components/Highlight/Highlight.jsx
+++ src/components/Highlight/Highlight.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 /**
  *
@@ -9,23 +9,23 @@ function Highlight() {
 
     const banner = {
         id: '7654567534',
-        name: 'Banner Título',
-        description: 'Essa é uma descrição breve do banner.',
-        url: 'https://placeholder.co/600x600',
+        name: 'Eat What You Cook With Us, Together!',
+        description: 'When you eat something that cooked by yourself, the happiness is priceless..',
+        url: '/Home/Highlight.png',
         link: '#'
     };
 
     return (
         <div className="banner__head">
             <div className="banner__wrapper" style={{ display: 'flex', alignItems: 'center' }}>
-                <div className="banner__content" style={{ flex: 1 }}>
+                <div className="banner__content">
                     <h1 className="banner__title">{banner.name}</h1>
                     <p className="banner__description">{banner.description}</p>
                     <a href={banner.link} className="banner__button">
                         <button>Saiba Mais</button>
                     </a>
                 </div>
-                <div className="banner__image-wrapper" style={{ flex: 1 }}>
+                <div className="banner__image-wrapper">
                     <img className="banner__image" src={banner.url} alt={banner.name} style={{ maxWidth: '100%' }} />
                 </div>
             </div>
